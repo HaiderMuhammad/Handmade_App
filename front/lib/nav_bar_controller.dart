d4013218cbfb46fba1757a48dafbba88102372ad
apple <103218645+HaiderMuhammad@users.noreply.github.com>
2022-11-04 14:15:53 +0300

    handmade

--- front/lib/nav_bar_controller.dart
+++ front/lib/nav_bar_controller.dart
@@ -1,9 +1,27 @@
+import 'package:front/api_call/product_api/api_methods.dart';
+import 'package:front/controllers/profile_controller.dart';
 import 'package:get/get.dart';
 
 class NavBarController extends GetxController {
   var selectedIndex = 0.obs;
+  ProfileController profileController = Get.find<ProfileController>();
+
+  @override
+  void onInit() {
+    profileController.getProfile();
+  }
 
   void changeIndex(int index){
     selectedIndex.value = index;
+  }
+
+  @override
+  void onReady() {
+
+  }
+
+  @override
+  void onClose() {
+
   }
 }
